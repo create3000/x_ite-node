90681e2f6744c8a1a9de66d8dcdb834ed0c5e6f5
Holger Seelig <holger.seelig@gmail.com>
2024-11-10 12:02:55 +0100

    Added require.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 // JSDOM
 
 const
-   { JSDOM }    = require ("jsdom"),
-   { window }   = new JSDOM (),
-   LocalStorage = require ("node-localstorage") .LocalStorage,
-   path         = require ("path"),
-   fs           = require ("fs"),
-   os           = require ("os");
+   { JSDOM }         = require ("jsdom"),
+   { window }       = new JSDOM (),
+   { LocalStorage } = require ("node-localstorage"),
+   path             = require ("path"),
+   fs               = require ("fs"),
+   os               = require ("os");
 
 const tmp = fs .mkdtempSync (path  .join (os .tmpdir (), "x_ite"));
 
-// LocalStorage
+window .require = require;
+
 
 Object .defineProperties (window,
 {
-   "localStorage": {
+   // require
+   require: require,
+   // storages
+   localStorage:
+   {
       value: new LocalStorage (path .join (tmp, "Local Storage")),
       configurable: true,
    },
-   "sessionStorage": {
+   sessionStorage:
+   {
       value: new LocalStorage (path .join (tmp, "Session Storage")),
       configurable: true,
    },
