4e6b8e624f2d5416227b0831a8e46e395baf5fcf
Holger Seelig <holger.seelig@gmail.com>
2024-11-10 11:34:17 +0100

    Added localStorage.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-
 // JSDOM
 
 const
-   { JSDOM }  = require ("jsdom"),
-   { window } = new JSDOM ();
+   { JSDOM }    = require ("jsdom"),
+   { window }   = new JSDOM (),
+   LocalStorage = require ("node-localstorage") .LocalStorage,
+   path         = require ("path"),
+   fs           = require ("fs"),
+   os           = require ("os");
+
+const tmp = fs .mkdtempSync (path  .join (os .tmpdir (), "x_ite"));
+
+// LocalStorage
+
+Object .defineProperties (window,
+{
+   "localStorage": {
+      value: new LocalStorage (path .join (tmp, "Local Storage")),
+      configurable: true,
+   },
+   "sessionStorage": {
+      value: new LocalStorage (path .join (tmp, "Session Storage")),
+      configurable: true,
+   },
+});
+
+// Window
 
 Object .assign (global, window);
 
