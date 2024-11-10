ec41bbb564ec26e2c56c1fd5d6c2e37859292169
Holger Seelig <holger.seelig@gmail.com>
2024-11-10 11:12:36 +0100

    Added jsdom.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 
-module .exports = { };
+// JSDOM
+
+const
+   { JSDOM }  = require ("jsdom"),
+   { window } = new JSDOM ();
+
+Object .assign (global, window);
+
+// X_ITE
+
+const X3D = require ("x_ite");
+
+module .exports = X3D;
