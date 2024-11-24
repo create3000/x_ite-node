7c116d98376367d790fc5c16861919557469ae81
Holger Seelig <holger.seelig@gmail.com>
2024-11-24 18:25:58 +0100

    Added isEnabled.

--- src/index.js
+++ src/index.js
@@ -345,6 +345,7 @@ const glFunctions = Object .fromEntries ([
    "createVertexArray",
    "drawBuffers",
    "generateMipmap",
+   "isEnabled",
    "readPixels",
    "renderbufferStorageMultisample",
    "texImage3D",
