33ba6c2fef5642b595ea4e20d5cab78b6f1ed4dc
Holger Seelig <holger.seelig@gmail.com>
2024-11-24 18:32:24 +0100

    Added polygonOffset.

--- src/index.js
+++ src/index.js
@@ -346,6 +346,7 @@ const glFunctions = Object .fromEntries ([
    "drawBuffers",
    "generateMipmap",
    "isEnabled",
+   "polygonOffset",
    "readPixels",
    "renderbufferStorageMultisample",
    "texImage3D",
