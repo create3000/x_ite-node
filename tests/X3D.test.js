478df3fa2f73f9a91f6df5f7a96d0de035bf0edd
Holger Seelig <holger.seelig@gmail.com>
2025-06-29 13:08:52 +0200

    Refactor tests for X3D and createBrowser for improved readability

--- tests/X3D.test.js
+++ tests/X3D.test.js
@@ -1,31 +1,31 @@
 
 const X3D = require ("..");
 
-// test ("X3D", () =>
-// {
-//    expect (X3D) .toBeInstanceOf (Object);
-// });
+test ("X3D", () =>
+{
+   expect (X3D) .toBeInstanceOf (Object);
+});
 
-// test ("createBrowser", () =>
-// {
-//    const canvas  = X3D .createBrowser ();
-//    const browser = canvas .browser;
+test ("createBrowser", () =>
+{
+   const canvas  = X3D .createBrowser ();
+   const browser = canvas .browser;
 
-//    expect (canvas) .toBeInstanceOf (HTMLElement);
-//    expect (browser) .toBeInstanceOf (X3D .X3DBrowser);
-// });
+   expect (canvas) .toBeInstanceOf (HTMLElement);
+   expect (browser) .toBeInstanceOf (X3D .X3DBrowser);
+});
 
-// test ("nodes", async () =>
-// {
-//    const canvas  = X3D .createBrowser ();
-//    const browser = canvas .browser;
-//    const scene   = browser .currentScene;
+test ("nodes", async () =>
+{
+   const canvas  = X3D .createBrowser ();
+   const browser = canvas .browser;
+   const scene   = browser .currentScene;
 
-//    await browser .loadComponents (browser .getProfile ("Full"));
+   await browser .loadComponents (browser .getProfile ("Full"));
 
-//    for (const ConcreteNode of browser .concreteNodes)
-//       expect (new ConcreteNode (scene)) .toBeInstanceOf (ConcreteNode);
-// });
+   for (const ConcreteNode of browser .concreteNodes)
+      expect (new ConcreteNode (scene)) .toBeInstanceOf (ConcreteNode);
+});
 
 test ("load one example", async () =>
 {
