a9ddd0b9850588c6171e73c5acc0b51ead0171b9
Holger Seelig <holger.seelig@gmail.com>
2026-06-16 09:55:59 +0200

    Added media examples tests.

--- tests/script.js
+++ tests/script.js
@@ -17,6 +17,7 @@ async function main ()
    await nodes ();
    await box ();
    await svg ();
+   await mediaExamples ();
 
    browser .dispose ();
 
@@ -46,6 +47,31 @@ async function svg ()
    const scene = await browser .createX3DFromURL (new X3D .MFString (url .pathToFileURL (path .join (__dirname, "Primitives.svg"))));
 
    fs .writeFileSync (path .join (__dirname, "Primitives.x3d"), scene .toXMLString ())
+}
+
+
+async function mediaExamples ()
+{
+   const
+      media    = `https://create3000.github.io/media/examples`,
+      response = await fetch (`${media}/config.json`),
+      examples = JSON .parse (await response .text ());
+
+   const canvas  = X3D .createBrowser ();
+   const browser = canvas .browser;
+
+   await browser .loadComponents (browser .getProfile ("Full"));
+
+   for (const { name, component } of examples)
+   {
+      console .log (component, name);
+
+      const scene = await browser .createX3DFromURL (new X3D .MFString (`${media}/${component}/${name}/${name}.x3d`));
+
+      scene .dispose ();
+   }
+
+   browser .dispose ();
 }
 
 main ();
